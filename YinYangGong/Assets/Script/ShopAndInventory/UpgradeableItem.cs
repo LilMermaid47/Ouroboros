62b702dd83e78c323c9e6d48dddb89e91d19d93e
Stephane Roberts <>
2022-10-06 14:30:22 -0400

    Added reason to deactivated bouton.

--- YinYangGong/Assets/Script/ShopAndInventory/UpgradeableItem.cs
+++ YinYangGong/Assets/Script/ShopAndInventory/UpgradeableItem.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewItemClass", menuName = "Item/Upgradeable")]
 public class UpgradeableItem : Item
 {
+    public UpgradeItemList upgradeItemList;
+    public float bonus = 0.1f;
+
+    private QuestManager questManager;
+    private UIController uIController;
+
     public override TypeOfItem ItemType()
     {
         return TypeOfItem.UpgradeItem;
@@ -12,7 +19,45 @@ public class UpgradeableItem : Item
 
     public override void UseItem()
     {
-        throw new System.NotImplementedException();
+        questManager = GameObject.FindGameObjectWithTag("QuestManager").GetComponent<QuestManager>();
+        uIController = GameObject.FindGameObjectWithTag("Canvas").GetComponent<UIController>();
+
+        switch (upgradeItemList) 
+        {
+            case UpgradeItemList.Shop:
+                break;
+            case UpgradeItemList.GoldenBuddhaStatue:
+                questManager.SetArgentBonus(bonus);
+                break;
+            case UpgradeItemList.JadeBuddhaStatue:
+                uIController.IncreaseMaxBalance((int)bonus);
+                break;
+            case UpgradeItemList.DisciplesSanctuary:
+                questManager.SetDiscipleBonus(bonus);
+                break;
+            case UpgradeItemList.BlueBuddhaStatue:
+                questManager.SetKiBonus(bonus);
+                break;
+            case UpgradeItemList.SusodasGarden:
+                break;
+            case UpgradeItemList.HuangseisWatchTower:
+                break;
+            case UpgradeItemList.Palisade:
+                questManager.AddReadiness(bonus);
+                break;
+            case UpgradeItemList.WoodenWall:
+                questManager.AddReadiness(bonus);
+                break;
+            case UpgradeItemList.StoneWall:
+                questManager.AddReadiness(bonus);
+                break;
+            case UpgradeItemList.SusodasStatue:
+                break;
+            case UpgradeItemList.HuangseisStatue:
+                break;
+            case UpgradeItemList.MothersTree:
+                break;
+        }
     }
 }
 public enum UpgradeItemList
